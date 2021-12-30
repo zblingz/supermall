170ea945d573606a0234b8c7010e83ff68846a10
star <moon@outlook.com>
2021-12-30 09:23:35 +0800

    'commit'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'Scroll',
+  props: {
+    probeType: {
+      type: Number,
+      default() {
+        return 0
+      }
+    }
+  },
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  mounted() {
+    //创建BScroll对象
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      probeType: this.probeType
+    }),
+    //监听滚动的位置
+    this.scroll.on('scroll', position => {
+      this.$emit('scroll', position)
+    })
+  },
+  methods: {
+    scrollTo(x, y, time=300) {
+      this.scroll.scrollTo(x, y, time)
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
